627801fd9a2920e5bb2654c13bdce06dab4de841
mininekos <manuel2g2m@gmail.com>
2022-11-11 14:43:29 +0100

    Mejora v1.0

--- src/app/directiva-personalizada.directive.ts
+++ src/app/directiva-personalizada.directive.ts
@@ -6,15 +6,20 @@ import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core'
 export class DirectivaPersonalizadaDirective {
 
   @HostBinding('class.estilo1') private ishovering!: boolean;
-  
-  constructor(private el: ElementRef) { }
-
-  @HostListener('mouseover') onMouseOver(){
-    if(this.el.nativeElement.tagName==='A')
-      this.ishovering=true;
+  private cadena:String;
+  constructor(private el: ElementRef) { 
+    this.cadena="";
   }
 
-  @HostListener('mouseout') onMouseOut(){
+  @HostListener('mouseover') ratonPasa(){
+    this.cadena=this.el.nativeElement.innerHTML;
+    this.el.nativeElement.innerHTML=this.el.nativeElement.innerHTML.toUpperCase();
+    this.ishovering=true;
+    
+  }
+
+  @HostListener('mouseout') ratonSale(){
     this.ishovering=false;
+    this.el.nativeElement.innerHTML=this.cadena;
   }
 }
